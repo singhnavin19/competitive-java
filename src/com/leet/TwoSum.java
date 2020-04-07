970c599ceb1ec729d480c3230b7d6da3840c639a
Navin Singh <navinsingh19@gmail.com>
2020-04-07 22:24:55 +0530

    Two No Sum By Using Map

--- src/com/leet/TwoSum.java
+++ src/com/leet/TwoSum.java
@@ -1,11 +1,29 @@
 package com.leet;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
 	public static void main(String[] args) {
+		int[] TwoNos=getTwoNoUsingMap(new int[] {1,2,7,9},9);
 		
-		System.out.println("hello");
-
+		System.out.println(Arrays.toString(TwoNos));
 	}
 
+	public static int[] getTwoNoUsingMap(int []input,int target ) {
+		Map<Integer, Integer> map=new HashMap<>();
+		
+		for(int no:input) {
+			int remains=target-no;
+			if(map.containsKey(remains)) {
+				return new int [] {map.get(remains),no};
+			}
+			map.put(no, no);
+		}
+		throw new IllegalArgumentException("No no found");
+	}
+	
+	
 }
